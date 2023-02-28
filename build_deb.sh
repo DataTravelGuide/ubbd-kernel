91a564dcfde75b19034bb33784d418ec57133cdf
Dongsheng Yang <dongsheng.yang.linux@gmail.com>
2023-02-28 11:35:42 +0000

    add more information in pkgname
    Signed-off-by: Dongsheng Yang <dongsheng.yang.linux@gmail.com>

--- build_deb.sh
+++ build_deb.sh
@@ -3,9 +3,12 @@
 set -e
 
 KERNEL_VERSION=`uname -r`
+ARCH=`dpkg --print-architecture`
 
 ./install_dep.sh
 
 sed "s/@KERNEL_VERSION@/${KERNEL_VERSION}/g" debian/ubbd-kernel.install.in > debian/ubbd-kernel.install
+sed "s/@KERNEL_VERSION@/${KERNEL_VERSION}/g" debian/changelog.in > debian/changelog
+sed "s/@ARCH@/${ARCH}/g" debian/control.in > debian/control
 git submodule update --init --recursive
 dpkg-buildpackage -uc -us
