eb689c87a81f7ee1c2a9a62d742f9c8bf8fd7dfb
Dongsheng Yang <dongsheng.yang@easystack.cn>
2023-06-16 05:19:49 +0000

    update build_deb.sh
    Signed-off-by: Dongsheng Yang <dongsheng.yang@easystack.cn>

--- build_deb.sh
+++ build_deb.sh
@@ -3,14 +3,20 @@
 set -e
 
 VER=`cat VERSION`
-KERNEL_VERSION=`uname -r`
-ARCH=`dpkg --print-architecture`
+arch=`dpkg --print-architecture`
 
 ./install_dep.sh
 
-sed "s/@KERNEL_VERSION@/${KERNEL_VERSION}/g" debian/ubbd-kernel.install.in > debian/ubbd-kernel.install
-sed "s/@KERNEL_VERSION@/${KERNEL_VERSION}/g" debian/changelog.in > debian/changelog
-sed -i "s/@VERSION@/${VER}/g" debian/changelog
-sed "s/@ARCH@/${ARCH}/g" debian/control.in > debian/control
 git submodule update --init --recursive
+
+source /etc/os-release
+
+sed "s/@ARCH@/${arch}/g" debian/control.in > debian/control
+sed "s/stable/${VERSION_CODENAME}/g" debian/changelog.in > debian/changelog
+sed -i "s/@VERSION@/${VER}/g" debian/changelog
+sed -i "s/@CODENAME@/${VERSION_CODENAME}/g" debian/changelog
+sed "s/@VERSION@/${VER}/g" debian/prerm.in > debian/prerm
+sed "s/@VERSION@/${VER}/g" debian/prerm.in > debian/prerm
+sed "s/@VERSION@/${VER}/g" debian/postinst.in > debian/postinst
+sed "s/@VERSION@/${VER}/g" dkms.conf.in > dkms.conf
 dpkg-buildpackage -uc -us
